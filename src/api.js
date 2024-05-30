5e628df6b5d18794ba7b778cbaf2180c97c7dd21
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-05-30 16:58:48 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -5,9 +5,6 @@ const API_URL = "http://3.223.98.72:1337/api/students";
 export const fetchStudents = async () => {
   try {
     const response = await fetch(API_URL);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
     const data = await response.json();
     return data;
   } catch (error) {
