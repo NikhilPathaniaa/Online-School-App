0c007a2ee69eb4541203778e657e44845bebbe99
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-05-31 10:29:24 +0530

    first commit

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -5,9 +5,9 @@ import { IoIosArrowDown } from "react-icons/io";
 const MainHeader = ({ searchQuery, setSearchQuery }) => {
   return (
     <div className="flex justify-between items-center mb-4 pt-3 p-6 pl-8 pb-0">
-      <div className="flex items-center space-x-2">
+      <div className="flex">
         <h1 className="text-2xl font-bold">Students</h1>
-        <span className="text-[#6754b3] w-9 rounded-full px-2 py-1 text-sm border border-[#6754b3]">82</span>
+        <span className="text-[#6754b3] rounded-3xl self-center ml-2.5 font-bold px-2 border border-[#6754b3]">82</span>
       </div>
       <div className="flex items-center space-x-4">
         {/* search start */}
@@ -15,13 +15,7 @@ const MainHeader = ({ searchQuery, setSearchQuery }) => {
           <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
             <CiSearch size={24} className="text-[#6754b3]" />
           </div>
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="block pt-2 ps-10  border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500 text-lg align-text-top"
-            placeholder="Search"
-          />
+          <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="block ps-10 w-80 border-b-2 py-1.5 focus:outline-none" placeholder="Search" />
         </div>
         {/* search end */}
         <button className="p-2 rounded-full">
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,23 +1,29 @@
 // src/components/Pagination.js
 import React from "react";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   return (
-    <div className="flex justify-between items-center mt-3">
-      <h1 className="text-white">_</h1>
-      <div>
-        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1} className="  bg-[#6754b3] mr-4  text-white w-20 text-2xl rounded-2xl ">
-          <IoIosArrowBack className="items-center" />
+    <div className="flex justify-between items-center my-5">
+      <div className="m-auto">
+        <button
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={(currentPage === 1 ? "border-[#999] text-slate-700 cursor-not-allowed" : "border-[#6754b3] text-white bg-[#6754b3]") + " mr-4 text-white w-14 rounded-2xl px-4 border"}>
+          &lt;
         </button>
 
-        <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages} className="bg-[#6754b3] text-white w-20 text-2xl rounded-2xl">
-          <IoIosArrowForward />
+        <button
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className={(currentPage === totalPages ? "border-[#999] text-slate-700 cursor-not-allowed" : "border-[#6754b3] text-white bg-[#6754b3]") + " mr-4 text-white w-14 rounded-2xl px-4 border"}>
+          &gt;
         </button>
       </div>
 
-      <span className="text-[#6754b3] border-[#6754b3] ">
-        <span className=" border border-[#6754b3] rounded-2xl ">{currentPage} </span> of {totalPages}
+      <span className="text-[#6754b3] flex gap-x-2.5 text-sm">
+        <span className="rounded-3xl px-3.5 border border-[#6754b3]">{currentPage} </span>
+        <span>of</span>
+        <span>{totalPages} </span>
       </span>
     </div>
   );
